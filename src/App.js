3d92d71166462e65a953b7cd3b8203674a993bcb
Bhawna198 <122960027+Bhawna198@users.noreply.github.com>
2025-09-13 13:47:37 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EndUserAnalytics from './analytics/EndUserAnalytics';
 import AdminAnalytics from './analytics/AdminAnalytics';
 import DashboardPage from './pages/DashboardPage';
+import BrowsePlans from './pages/BrowsePlans';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
          <Route path="/" element={<DashboardPage />} />
         <Route path="/user/dashboard" element={<EndUserAnalytics />} />
         <Route path="/admin/dashboard" element={<AdminAnalytics />} />
+         <Route path="/Plans" element={<BrowsePlans />} />
       </Routes>
     </BrowserRouter>
   );
